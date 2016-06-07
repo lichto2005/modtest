d790d44df2b9d9caf026f72ff42efd5d596b40ca
lichto2005 <lichto2005@gmail.com>
2016-06-06 22:34:08 -0400

    recommit after changing project structure

--- src/main/java/com/lichto/modtest/ModTest.java
+++ src/main/java/com/lichto/modtest/ModTest.java
@@ -1,24 +1,33 @@
 package com.lichto.modtest;
 
+import com.lichto.modtest.configuration.ConfigurationHandler;
+import com.lichto.modtest.proxy.IProxy;
+import com.lichto.modtest.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import javax.security.auth.login.Configuration;
 
 /**
  * Created by Noah Lichtenstein on 6/5/2016.
  */
 
-@Mod(modid="ModTest", name="ModTest", version="1.7.10-1.0")
+@Mod(modid= Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.VERSION)
 public class ModTest
 {
-    @Mod.Instance("ModTest")
+    @Mod.Instance(Reference.MOD_ID)
     public static ModTest instance;
+
+    @SidedProxy(clientSide=Reference.CLIENT_PROXY, serverSide=Reference.SERVER_PROXY)
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
     }
 
     @Mod.EventHandler
---
+++ src/main/java/com/lichto/modtest/reference/Reference.java
@@ -0,0 +1,13 @@
+package com.lichto.modtest.reference;
+
+/**
+ * Created by Noah Lichtenstein on 6/5/2016.
+ */
+public class Reference
+{
+    public static final String MOD_ID = "ModTest";
+    public static final String MOD_NAME = "Mod Test";
+    public static final String VERSION = "1.0";
+    public static final String SERVER_PROXY = "com.lichto.modtest.proxy.ServerProxy";
+    public static final String CLIENT_PROXY = "com.lichto.modtest.proxy.ClientProxy";
+}
